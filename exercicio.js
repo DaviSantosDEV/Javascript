23a0cd41dc49dc2783c60c9ded74fd464be3c02e
Davi Santos <102557963+DaviSantosDEV@users.noreply.github.com>
2022-05-04 16:46:18 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,38 @@
+//1-a
+console.log(10+20*30);
+
+//1-b
+
+console.log(4**2/30);
+
+//1-c
+console.log((9**4)*6-1);
+
+//2-a
+console.log(10%3*10**1+1-10*4/2)
+
+//3-a
+console.log('Davi Santos')
+
+//3-b
+const x = 3;
+const y = 5;
+
+console.log(2*x*3*y)
+
+//3-c
+
+const a = 10
+
+const b = 40
+
+const c = 24
+
+console.log (a+b+c)
+
+//3-d
+
+const valor = 1800
+const aumento = 1800
+console.log (valor+aumento)
+
